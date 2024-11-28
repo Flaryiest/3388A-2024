e4ec7c22ab8281ee30930e42b3894b117b6ffafb
Eric Zuo <ericmzuo1@gmail.com>
2024-11-27 18:56:42 -0700

    reset PID to default

--- src/main.cpp
+++ src/main.cpp
@@ -39,16 +39,15 @@ lemlib::ControllerSettings lateral_controller(10,
                                               20
 );
 
-
-lemlib::ControllerSettings angular_controller(10, // proportional gain (kP)
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              3, // derivative gain (kD)
+                                              10, // derivative gain (kD)
                                               3, // anti windup
-                                              1, // small error range, in inches
+                                              1, // small error range, in degrees
                                               100, // small error range timeout, in milliseconds
-                                              3, // large error range, in inches
+                                              3, // large error range, in degrees
                                               500, // large error range timeout, in milliseconds
-                                              20 // maximum acceleration (slew)
+                                              0 // maximum acceleration (slew)
 );
 
 lemlib::Chassis chassis(drivetrain,
