26c90e6e336714b6cf3cb0993943cb8ef4d31d41
Eric Zuo <ericmzuo1@gmail.com>
2024-11-29 17:31:52 -0700

    autonomous tuning

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include "main.h"
 #include "lemlib/api.hpp"
+#include "pros/llemu.hpp"
 #include "pros/misc.h"
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 pros::MotorGroup left_motors({-15,-6, -7}, pros::MotorGearset::red);
 pros::MotorGroup right_motors({3, 5, 4}, pros::MotorGearset::red);
-pros::Motor intake(-18, pros::MotorGearset::red);
+pros::Motor intake(-18, pros::MotorGearset::green);
 pros::adi::DigitalOut clamp('F');
-pros::adi::DigitalOut wing('E');
-pros::adi::DigitalIn wingPower('E');
+pros::adi::DigitalOut wing('D', false);
+lemlib::ExpoDriveCurve throttle_curve(3, // joystick deadband out of 127
+                                     10, // minimum output where drivetrain will move out of 127
+                                     1.019 // expo curve gain
+);
+
+// input curve for steer input during driver control
+lemlib::ExpoDriveCurve steer_curve(3, // joystick deadband out of 127
+                                  10, // minimum output where drivetrain will move out of 127
+                                  1.009 // expo curve gain
+);
 
 lemlib::Drivetrain drivetrain(&left_motors,
                               &right_motors,
@@ -40,20 +50,22 @@ lemlib::ControllerSettings lateral_controller(10,
 );
 
 lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
-                                              0.002, // integral gain (kI)
+                                              0.0015, // integral gain (kI)
                                               10, // derivative gain (kD)
-                                              0, // anti windup
-                                              0, // small error range, in degrees
-                                              0, // small error range timeout, in milliseconds
-                                              0, // large error range, in degrees
-                                              0, // large error range timeout, in milliseconds
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
 
 lemlib::Chassis chassis(drivetrain,
                         lateral_controller,
                         angular_controller,
-                        sensors
+                        sensors,
+                        &throttle_curve,
+                        &steer_curve
 );
 
 void on_center_button() {
@@ -69,27 +81,74 @@ void on_center_button() {
 void initialize() {
 	pros::lcd::initialize();
 	chassis.calibrate();
-    autonomous();
 }
 
 void disabled() {}
 
 void competition_initialize() {}
 
-void autonomous() {
+void redSideLeft() {
     chassis.setPose(0, 0, 0);
     clamp.set_value(HIGH);
-    chassis.moveToPoint(0, -19, 2000, {.forwards = false});
+    chassis.moveToPoint(0, -19, 2000, {.forwards = false, .maxSpeed = 100});
     pros::delay(900); 
     clamp.set_value(LOW);
     pros::delay(500);
     intake.move(127);
     pros::delay(1400);
     clamp.set_value(HIGH);
-
 }
 
+void redSideRight() {
+    chassis.setPose(0, 0, 0);
+    clamp.set_value(HIGH);
+    chassis.moveToPoint(0, -20.0, 2000, {.forwards = false, .maxSpeed = 80});
+    chassis.moveToPoint(0, -25.5, 2000, {.forwards = false, .maxSpeed = 20});
+    pros::delay(1000); 
+    clamp.set_value(LOW);
+    pros::delay(500);
+    intake.move(127);
+    pros::delay(1400);
+    clamp.set_value(HIGH);
+}
+
+void blueSideLeft() {
+    chassis.setPose(0, 0, 0);
+    clamp.set_value(HIGH);
+    chassis.moveToPoint(0, -19, 2000, {.forwards = false, .maxSpeed = 100});
+    pros::delay(900); 
+    clamp.set_value(LOW);
+    pros::delay(500);
+    intake.move(127);
+    pros::delay(1400);
+    clamp.set_value(HIGH);
+}
+
+void blueSideRight() {
+    chassis.setPose(0, 0, 0);
+    clamp.set_value(HIGH);
+    chassis.moveToPoint(0, -19, 2000, {.forwards = false, .maxSpeed = 100});
+    pros::delay(900); 
+    clamp.set_value(LOW);
+    pros::delay(500);
+    intake.move(127);
+    pros::delay(1400);
+    clamp.set_value(HIGH);
+}
+
+
+
+
+
+
+void autonomous() {
+    redSideRight();
+}
+
+
+
 void opcontrol() {
+    bool wingState = false;
     while (true) {
         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
@@ -98,6 +157,7 @@ void opcontrol() {
 		bool clampButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
 		bool clampReverseButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
         bool wingButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
+        
 
 		if (intakeButton) {
 			intake.move(127);
@@ -117,16 +177,14 @@ void opcontrol() {
 		}
 
         if (wingButton) {
-            if (wingPower.get_value() > 0) {
-                wing.set_value(HIGH);
-            }
-            else {
-                wing.set_value(LOW);
-            }
+            wingState = !wingState;
+            wing.set_value(wingState ? HIGH : LOW);
+            pros::delay(200);
+}
 
-        }
+
         int leftX = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
-        // chassis.arcade(leftY, rightX);
+        chassis.arcade(leftY, rightX);
 
         pros::delay(25);
 		
