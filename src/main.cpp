cc2d23305b44d60fb02a16baa510ac4fb4b2a707
Eric Zuo <ericmzuo1@gmail.com>
2025-10-17 22:11:49 -0600

    nigga
    nigga

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,14 @@ pros::Motor leftIntakeBottom(15, pros::MotorGearset::blue);
 pros::Motor leftIntakeTop(16, pros::MotorGearset::green);
 pros::Motor rightIntakeBottom(17, pros::MotorGearset::green);
 pros::Vision wallstake_sensor(5);
-pros::vision_signature_s_t red_ring_sig = pros::Vision::signature_from_utility(1, 237, 61, 74, 0, 0, 0, 10.0, 0);
-pros::vision_signature_s_t blue_ring_sig = pros::Vision::signature_from_utility(3, 13, 114, 227, 0, 0, 0, 10.0, 0);
+pros::vision_signature_s_t red_ring_sig = pros::Vision::signature_from_utility(1, 237, 61, 74, 0, 0, 0, 10.0, 3.0);
+pros::vision_signature_s_t blue_ring_sig = pros::Vision::signature_from_utility(3, 13, 114, 227, 0, 0, 0, 10.0, 3.0);
+
+pros::Distance park_distance(10);
 
 pros::adi::DigitalOut intake('A', true); 
 pros::adi::DigitalOut doinker('B', true); 
-
+pros::adi::DigitalOut expansion('C', false);
 lemlib::ExpoDriveCurve throttle_curve(5,
                                      10,
                                      1.019 
@@ -83,9 +85,106 @@ lemlib::Chassis chassis(drivetrain,
                         &steer_curve
 );
 
-void red_left_auton() {
+void left_auton() {
+    chassis.setPose(0, 0, 0);
+    intake.set_value(false);
+    leftIntakeBottom.move(-127); //bottm
+    leftIntakeTop.move(-127); //middle
+    rightIntakeBottom.move(127); //top
+    chassis.moveToPoint(0, 19.0, 2000, {.forwards = true, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(1000);
+    chassis.moveToPoint(-7, 29.0, 2000, {.forwards = true, .maxSpeed = 40, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(2000);
+    //chassis.moveToPoint(-10, 34.0, 2000, {.forwards = true, .maxSpeed = 80, .minSpeed = 10, .earlyExitRange = 0.01});
+    //pros::delay(1500);
+    chassis.moveToPoint(-5, 27.5, 2000, {.forwards = false, .maxSpeed = 50, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(250);
+    chassis.turnToHeading(
+    225,
+    4000,
+    {.maxSpeed = 50}, // will never exceed 50
+    false 
+    );
+    pros::delay(250);
+    chassis.moveToPoint(8.8, 32.7, 2000, {.forwards = false, .maxSpeed = 100, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(500);
+    rightIntakeBottom.move(-127); //top
+    pros::delay(2000);
+    rightIntakeBottom.move(0);
 
+    chassis.moveToPoint(-19.8, 24, 2000, {.forwards = true, .maxSpeed = 70, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(500);
+    
+    doinker.set_value(false);
+    pros::delay(200);
+    chassis.moveToPoint(-27.3, 12, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 10, .earlyExitRange = 0.01});
+    rightIntakeBottom.move(127); //top
+    pros::delay(3000);
+    chassis.moveToPoint(-26, 20, 2000, {.forwards = false, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(1000);
+    chassis.moveToPoint(-18, 30.5, 2000, {.forwards = false, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(500);
+    rightIntakeBottom.move(-127);
+        leftIntakeBottom.move(127); //bottm
+    leftIntakeTop.move(127); //middle
+    doinker.set_value(true);
+    intake.set_value(true);
+    pros::delay(200);
+            leftIntakeBottom.move(-127); //bottm
+    leftIntakeTop.move(-127); //middle
+    rightIntakeBottom.move(127);
 }
+
+void right_auton() {
+    chassis.setPose(0, 0, 0);
+    intake.set_value(false);
+    leftIntakeBottom.move(-127); //bottm
+    leftIntakeTop.move(-127); //middle
+    rightIntakeBottom.move(127); //top
+    chassis.moveToPoint(0, 19, 2000, {.forwards = true, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(1000);
+    chassis.moveToPoint(6, 29.0, 2000, {.forwards = true, .maxSpeed = 38, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(2000);
+    //chassis.moveToPoint(-10, 34.0, 2000, {.forwards = true, .maxSpeed = 80, .minSpeed = 10, .earlyExitRange = 0.01});
+    //pros::delay(1500);
+    chassis.moveToPoint(5, 27.5, 2000, {.forwards = false, .maxSpeed = 50, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(250);
+    chassis.turnToHeading(
+    315,
+    4000,
+    {.maxSpeed = 50}, // will never exceed 50
+    false 
+    );
+    pros::delay(250);
+    chassis.moveToPoint(-8.8, 31, 2000, {.forwards = false, .maxSpeed = 100, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(500);
+    //rightIntakeBottom.move(-127); //top
+    pros::delay(1000);
+    rightIntakeBottom.move(0);
+
+    chassis.moveToPoint(17.4, 24.5, 2000, {.forwards = true, .maxSpeed = 70, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(500);
+    
+    doinker.set_value(false);
+    pros::delay(200);
+    chassis.moveToPoint(25.2, 11, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 10, .earlyExitRange = 0.01});
+    rightIntakeBottom.move(127); //top
+    pros::delay(3000);
+    chassis.moveToPoint(23.9, 19.5, 2000, {.forwards = false, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(1000);
+    chassis.moveToPoint(17.9, 29.8, 2000, {.forwards = false, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(500);
+    rightIntakeBottom.move(-127);
+        leftIntakeBottom.move(127); //bottm
+    leftIntakeTop.move(127); //middle
+    doinker.set_value(true);
+    intake.set_value(true);
+    pros::delay(200);
+            leftIntakeBottom.move(-127); //bottm
+    leftIntakeTop.move(-127); //middle
+    rightIntakeBottom.move(127);  
+}
+
 void good_auton() {
     
 }
@@ -130,67 +229,25 @@ void skillsAutonomous() {
 }
 
 void autonomous() {
-    red_left_auton();
-    chassis.setPose(0, 0, 0);
-    intake.set_value(false);
-    leftIntakeBottom.move(-127); //bottm
-    leftIntakeTop.move(-127); //middle
-    rightIntakeBottom.move(127); //top
-    chassis.moveToPoint(0, 19.0, 2000, {.forwards = true, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
-    pros::delay(1000);
-    chassis.moveToPoint(-7, 29.0, 2000, {.forwards = true, .maxSpeed = 40, .minSpeed = 10, .earlyExitRange = 0.01});
-    pros::delay(2000);
-    //chassis.moveToPoint(-10, 34.0, 2000, {.forwards = true, .maxSpeed = 80, .minSpeed = 10, .earlyExitRange = 0.01});
-    //pros::delay(1500);
-    chassis.moveToPoint(-5, 27.5, 2000, {.forwards = false, .maxSpeed = 50, .minSpeed = 10, .earlyExitRange = 0.01});
-    pros::delay(250);
-    chassis.turnToHeading(
-    225,
-    4000,
-    {.maxSpeed = 50}, // will never exceed 50
-    false 
-    );
-    pros::delay(250);
-    chassis.moveToPoint(8.8, 32.7, 2000, {.forwards = false, .maxSpeed = 100, .minSpeed = 10, .earlyExitRange = 0.01});
-    pros::delay(500);
-    rightIntakeBottom.move(-127); //top
-    pros::delay(2000);
-    rightIntakeBottom.move(0);
+    left_auton();
 
-    chassis.moveToPoint(-18, 23.5, 2000, {.forwards = true, .maxSpeed = 70, .minSpeed = 10, .earlyExitRange = 0.01});
-    pros::delay(500);
-    
-    doinker.set_value(false);
-    pros::delay(200);
-    chassis.moveToPoint(-26.5, 14, 2000, {.forwards = true, .maxSpeed = 127, .minSpeed = 10, .earlyExitRange = 0.01});
-    rightIntakeBottom.move(127); //top
-    pros::delay(3000);
-    chassis.moveToPoint(-27, 20, 2000, {.forwards = false, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
-    pros::delay(1000);
-    chassis.turnToHeading(
-    180,
-    4000,
-    {.maxSpeed = 50}, // will never exceed 50
-    false 
-    );
-    doinker.set_value(true);
-    chassis.moveToPoint(-27, 40, 2000, {.forwards = false, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
-    
-    
 }
 
 
 void opcontrol() {
     // Persistent state for intake piston toggle
-    static bool intakePistonState = false;      // false = retracted, true = extended
-    static bool prevIntakeFour = false;         // previous loop state of R2 button
+    static bool intakePistonState = true;      // false = retracted, true = extended
+    static bool prevIntakeFour = true;         // previous loop state of R2 button
     // Persistent state for doinker toggle
-    static bool doinkerState = false;           // false = retracted, true = extended
+    static bool doinkerState = true;           // false = retracted, true = extended
     static bool prevDoinkerButton = true;      // previous loop state of doinker button (A)
+    // Persistent state for expansion toggle
+    static bool expansionState = false;        // false = retracted, true = extended (deployed)
+    static bool prevExpansionButton = false;   // previous loop state of expansion button (B)
     
     // Color sort config: 1 = red, 3 = blue
     // Change this based on which alliance you're on
-    int colorToReject = 1; // Reject blue rings (keep red). Change to 1 to reject red rings (keep blue)
+    int colorToReject = 1; // Reject red rings (keep blue). Change to 3 to reject blue rings (keep red)
     
     while (true) {
         bool intakeOne = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
@@ -198,18 +255,53 @@ void opcontrol() {
         bool intakeThree = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
         bool intakeFour = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
         bool doinkerButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
+        bool expansionButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_B);
         bool intakeLiftState = false; // unused currently
         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
-        // Get the largest object detected by the vision sensor
-        wallstake_sensor.set_signature(1, &red_ring_sig);
-        wallstake_sensor.set_signature(3, &blue_ring_sig);
-
-        pros::vision_object_s_t obj = wallstake_sensor.get_by_size(0);
+        // Get object count to see if anything is detected
+        int32_t obj_count = wallstake_sensor.get_object_count();
+        
+        // Initialize object with default values
+        pros::vision_object_s_t obj;
+        obj.signature = 0;
+        obj.width = 0;
+        obj.height = 0;
+        
+        // Only get object if count is valid and greater than 0
+        if (obj_count > 0 && obj_count < 100) {
+            obj = wallstake_sensor.get_by_size(0);
+        }
+        
+        // Debug: Display vision sensor data on brain screen (update every 200ms to avoid spam)
+        static uint32_t last_update = 0;
+        static int counter = 0;
+        if (pros::millis() - last_update > 200) {
+            counter++; // Shows screen is updating
+            pros::lcd::set_text(0, "Vision Debug [" + std::to_string(counter) + "]");
+            pros::lcd::set_text(1, "Objects: " + std::to_string(obj_count));
+            pros::lcd::set_text(2, "Sig: " + std::to_string(obj.signature));
+            pros::lcd::set_text(3, "W: " + std::to_string(obj.width) + " H: " + std::to_string(obj.height));
+            pros::lcd::set_text(4, "Reject: " + std::to_string(colorToReject));
+            
+            // Check if we have a valid detection
+            if (obj_count <= 0 || obj_count >= 100) {
+                pros::lcd::set_text(5, "SENSOR ERROR");
+            } else if (obj.signature == 255 || obj.signature == 0) {
+                pros::lcd::set_text(5, "NO OBJECT DETECTED");
+            } else {
+                pros::lcd::set_text(5, "Sig " + std::to_string(obj.signature) + " detected!");
+            }
+            
+            last_update = pros::millis();
+        }
         
         // Color sort: if the largest object matches colorToReject, reverse rightIntakeBottom briefly
-        if (obj.signature == colorToReject && obj.width > 3 && obj.height > 3) {
+        // Only reject if we have a valid signature and object count
+        if (obj_count > 0 && obj_count < 100 && obj.signature == colorToReject && 
+            obj.signature != 255 && obj.signature != 0 && obj.width > 3 && obj.height > 3) {
+            pros::lcd::set_text(6, "REJECTING!");
             rightIntakeBottom.move(-127);
             pros::delay(200);
             rightIntakeBottom.move(0);
@@ -231,21 +323,7 @@ void opcontrol() {
             leftIntakeTop.move(127);
             rightIntakeBottom.move(-127);
 
-        } else if (intakeFour) {
-            // Edge-triggered toggle: only toggle when button transitions from not pressed to pressed
-            if (!prevIntakeFour) {
-                intakePistonState = !intakePistonState;
-                intake.set_value(intakePistonState);
-                pros::delay(100);
-            }
-        } else if (doinkerButton) {
-            // Edge-triggered toggle for doinker
-            if (!prevDoinkerButton) {
-                doinkerState = !doinkerState;
-                doinker.set_value(doinkerState);
-                pros::delay(100);
-            }
-        }
+        } 
 
         else {
             leftIntakeBottom.move(0);
@@ -253,9 +331,38 @@ void opcontrol() {
             rightIntakeBottom.move(0);
         }
 
+        if (intakeFour) {
+            // Edge-triggered toggle: only toggle when button transitions from not pressed to pressed
+            if (!prevIntakeFour) {
+                intakePistonState = !intakePistonState;
+                intake.set_value(intakePistonState);
+                pros::delay(100);
+            }
+        } 
+        if (doinkerButton) {
+            // Edge-triggered toggle for doinker
+            if (!prevDoinkerButton) {
+                doinkerState = !doinkerState;
+                doinker.set_value(doinkerState);
+                pros::delay(100);
+            }
+        }
+        
+        if (expansionButton) {
+            // Edge-triggered toggle for expansion
+            if (!prevExpansionButton) {
+                expansionState = !expansionState;
+                expansion.set_value(expansionState);
+                pros::delay(100);
+            }
+        }
+
+
+
         chassis.arcade(leftY, rightX);
         prevIntakeFour = intakeFour; // update edge detector
         prevDoinkerButton = doinkerButton; // update edge detector for doinker
+        prevExpansionButton = expansionButton; // update edge detector for expansion
         pros::delay(5);
     }
 }
