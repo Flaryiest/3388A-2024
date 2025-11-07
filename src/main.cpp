7d91b6f9069deb468ffb1b95af85397c8da9cb34
Eric Zuo <ericmzuo1@gmail.com>
2025-11-07 14:23:35 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,7 @@ pros::MotorGroup right_motors({10, 9, 8}, pros::MotorGearset::blue);
 pros::Motor leftIntakeBottom(15, pros::MotorGearset::blue);
 pros::Motor leftIntakeTop(16, pros::MotorGearset::green);
 pros::Motor rightIntakeBottom(17, pros::MotorGearset::green);
-pros::Vision wallstake_sensor(5);
-pros::vision_signature_s_t red_ring_sig = pros::Vision::signature_from_utility(1, 237, 61, 74, 0, 0, 0, 10.0, 3.0);
-pros::vision_signature_s_t blue_ring_sig = pros::Vision::signature_from_utility(3, 13, 114, 227, 0, 0, 0, 10.0, 3.0);
+pros::Optical color_sensor(7);
 
 pros::Distance park_distance(10);
 
@@ -42,7 +40,7 @@ lemlib::Drivetrain drivetrain(&left_motors,
                               2
 );
 
-pros::Imu imu(5);
+pros::Imu imu(13);
 pros::Rotation horizontal_encoder(4);
 
 lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_2, 0.2);
@@ -136,6 +134,23 @@ void left_auton() {
     rightIntakeBottom.move(127);
 }
 
+void left_auton_v2() {
+    chassis.setPose(0, 0, 0);
+    intake.set_value(false);
+    expansion.set_value(true);
+    leftIntakeBottom.move(-127); //bottm
+    leftIntakeTop.move(-127); //middle
+    rightIntakeBottom.move(-127); //top   
+    chassis.moveToPoint(0, 24, 2000, {.forwards = true, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(1000);
+    chassis.moveToPoint(0, 5, 2000, {.forwards = false, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});
+    pros::delay(500); 
+    chassis.moveToPoint(3, 0, 2000, {.forwards = true, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});   
+    pros::delay(500);
+    rightIntakeBottom.move(127); //top
+    chassis.moveToPoint(-30, 0, 2000, {.forwards = false, .maxSpeed = 120, .minSpeed = 10, .earlyExitRange = 0.01});  
+}
+
 void right_auton() {
     chassis.setPose(0, 0, 0);
     expansion.set_value(true);
@@ -216,8 +231,7 @@ void initialize() {
     pros::c::motor_set_brake_mode(1, pros::E_MOTOR_BRAKE_HOLD);
     
     // Configure vision sensor signatures for color sorting
-    wallstake_sensor.set_signature(1, &red_ring_sig);
-    wallstake_sensor.set_signature(3, &blue_ring_sig);
+
 
 }
 
@@ -231,7 +245,7 @@ void skillsAutonomous() {
 }
 
 void autonomous() {
-    left_auton();
+    left_auton_v2();
 
 }
 
@@ -246,16 +260,20 @@ void opcontrol() {
     // Persistent state for expansion toggle
     static bool expansionState = true;        // false = retracted, true = extended (deployed)
     static bool prevExpansionButton = true;   // previous loop state of expansion button (B)
+    // Persistent state for color sort toggle
+    static bool prevColorSortButton = true;   // previous loop state of color sort button (X)
     
-    // Color sort config: 1 = red, 3 = blue
-    // Change this based on which alliance you're on
-    int colorToReject = 1; // Reject red rings (keep blue). Change to 3 to reject blue rings (keep red)
-    
+    // Color sort config: 1 = red, 2 = blue
+    int colorToReject = 1; 
+    static bool colorSortMode = false; // false = off, true = on
     while (true) {
         bool intakeOne = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
         bool intakeTwo = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
         bool intakeThree = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
         bool intakeFour = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
+        bool intakeFive = controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y);
+
+        bool colorSortButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_X);
         bool doinkerButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
         bool expansionButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_B);
         bool intakeLiftState = false; // unused currently
@@ -263,51 +281,9 @@ void opcontrol() {
         int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
         // Get object count to see if anything is detected
-        int32_t obj_count = wallstake_sensor.get_object_count();
-        
-        // Initialize object with default values
-        pros::vision_object_s_t obj;
-        obj.signature = 0;
-        obj.width = 0;
-        obj.height = 0;
-        
-        // Only get object if count is valid and greater than 0
-        if (obj_count > 0 && obj_count < 100) {
-            obj = wallstake_sensor.get_by_size(0);
-        }
-        
-        // Debug: Display vision sensor data on brain screen (update every 200ms to avoid spam)
-        static uint32_t last_update = 0;
-        static int counter = 0;
-        if (pros::millis() - last_update > 200) {
-            counter++; // Shows screen is updating
-            pros::lcd::set_text(0, "Vision Debug [" + std::to_string(counter) + "]");
-            pros::lcd::set_text(1, "Objects: " + std::to_string(obj_count));
-            pros::lcd::set_text(2, "Sig: " + std::to_string(obj.signature));
-            pros::lcd::set_text(3, "W: " + std::to_string(obj.width) + " H: " + std::to_string(obj.height));
-            pros::lcd::set_text(4, "Reject: " + std::to_string(colorToReject));
-            
-            // Check if we have a valid detection
-            if (obj_count <= 0 || obj_count >= 100) {
-                pros::lcd::set_text(5, "SENSOR ERROR");
-            } else if (obj.signature == 255 || obj.signature == 0) {
-                pros::lcd::set_text(5, "NO OBJECT DETECTED");
-            } else {
-                pros::lcd::set_text(5, "Sig " + std::to_string(obj.signature) + " detected!");
-            }
-            
-            last_update = pros::millis();
-        }
-        
-        // Color sort: if the largest object matches colorToReject, reverse rightIntakeBottom briefly
-        // Only reject if we have a valid signature and object count
-        if (obj_count > 0 && obj_count < 100 && obj.signature == colorToReject && 
-            obj.signature != 255 && obj.signature != 0 && obj.width > 3 && obj.height > 3) {
-            pros::lcd::set_text(6, "REJECTING!");
-            rightIntakeBottom.move(-127);
-            pros::delay(200);
-            rightIntakeBottom.move(0);
-        }
+
+        double hue = color_sensor.get_hue();
+
         
         if (intakeOne) {
             leftIntakeBottom.move(-127); //bottm
@@ -324,8 +300,11 @@ void opcontrol() {
             leftIntakeBottom.move(127);
             leftIntakeTop.move(127);
             rightIntakeBottom.move(127);
-
-        } 
+        } else if (intakeFive) {
+            leftIntakeBottom.move(0);
+            leftIntakeTop.move(127);
+            rightIntakeBottom.move(-127);
+        }
 
         else {
             leftIntakeBottom.move(0);
@@ -333,12 +312,30 @@ void opcontrol() {
             rightIntakeBottom.move(0);
         }
 
+        if (colorSortMode) {
+            if (hue >= 330 || hue <= 30) { // Red hue range
+                if (colorToReject == 1) {
+                    leftIntakeBottom.move(-127); //bottm
+                    leftIntakeTop.move(-127); //middle
+                    rightIntakeBottom.move(127); //top
+                    pros::delay(150);
+                }
+            } else if (hue >= 180 && hue <= 250) { // Blue hue range
+                if (colorToReject == 2) {
+                    leftIntakeBottom.move(-127); //bottm
+                    leftIntakeTop.move(-127); //middle
+                    rightIntakeBottom.move(127); //top
+                    pros::delay(150);
+                }
+            }
+        }
+
         if (intakeFour) {
             // Edge-triggered toggle: only toggle when button transitions from not pressed to pressed
             if (!prevIntakeFour) {
                 intakePistonState = !intakePistonState;
                 intake.set_value(intakePistonState);
-                pros::delay(100);
+                pros::delay(100); 
             }
         } 
         if (doinkerButton) {
@@ -358,6 +355,15 @@ void opcontrol() {
                 pros::delay(100);
             }
         }
+        
+        if (colorSortButton) {
+            // Edge-triggered toggle for color sorting
+            if (!prevColorSortButton) {
+                colorSortMode = !colorSortMode;
+                pros::delay(100);
+            }
+        }
+
 
 
 
@@ -365,6 +371,7 @@ void opcontrol() {
         prevIntakeFour = intakeFour; // update edge detector
         prevDoinkerButton = doinkerButton; // update edge detector for doinker
         prevExpansionButton = expansionButton; // update edge detector for expansion
+        prevColorSortButton = colorSortButton; // update edge detector for color sort
         pros::delay(5);
     }
 }
